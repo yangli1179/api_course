8ee0301e73b2586d5d9ade91792f14bc96753a75
yangli1179 <1179384105@qq.com>
2020-03-13 16:34:58 +0800

    2020031301_2

--- course/day01/list_test.py
+++ course/day01/list_test.py
@@ -1,48 +1,59 @@
-# -*- coding: utf-8 -*-
-"""
-
-=================================
-Author: yangli
-Created on: 2020/3/13
-
-E-mail:1179384105@qq.com
-
-=================================
-
-
-"""
-
-# a = [1, 2, 3, 'a4']
-# a.insert(2, 'b')
+# # -*- coding: utf-8 -*-
+# """
+#
+# =================================
+# Author: yangli
+# Created on: 2020/3/13
+#
+# E-mail:1179384105@qq.com
+#
+# =================================
+#
+#
+# """
+#
+# # a = [1, 2, 3, 'a4']
+# # a.insert(2, 'b')
+# # print(a)
+#
+# # b = ['ab1', 'ac1']
+# # b.reverse()
+# # print(min(b))
+# # print(b)
+# c =[1, 22, 32, 5, 6, 8, 11, 12]
+# # c.sort(reverse=True)
+# # c.reverse()
+# # print(c)
+# # print(c[1])
+# # print(c[-1])
+#
+# a = c[0:-1:2]
 # print(a)
+#
+#
+# # c = [4, 5, 6]
+# # b.extend(c)
+# # b.append('d')
+# # b.pop()
+# # b.pop(1)
+# # del b[1]
+# # c = b.count(8)
+# # print(b)
+# # print(c)
+# # print(len(b))
 
-# b = ['ab1', 'ac1']
-# b.reverse()
-# print(min(b))
-# print(b)
-c =[1, 22, 32, 5, 6, 8, 11, 12]
-# c.sort(reverse=True)
-# c.reverse()
-# print(c)
-# print(c[1])
-# print(c[-1])
-
-a = c[0:-1:2]
+# 练习
+a = [1, 2, 3, 4, 5, 6, 7, 8]
+b = [11, 12, 13]
+a.insert(0,'0')
 print(a)
 
+a.extend(b)
+print(a)
 
-# c = [4, 5, 6]
-# b.extend(c)
-# b.append('d')
-# b.pop()
-# b.pop(1)
-# del b[1]
-# c = b.count(8)
-# print(b)
-# print(c)
-# print(len(b))
-
-
-
+a.append('9')
+print(a)
+a.pop(-1)
+print(a)
 
 
