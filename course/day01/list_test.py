5fb00811b829fee5c1cde13b77938c09e97a6b38
yangli1179 <1179384105@qq.com>
2020-03-13 16:23:46 +0800

    2020031301_2

--- course/day01/list_test.py
+++ course/day01/list_test.py
@@ -12,3 +12,37 @@ E-mail:1179384105@qq.com
 
 """
 
+# a = [1, 2, 3, 'a4']
+# a.insert(2, 'b')
+# print(a)
+
+# b = ['ab1', 'ac1']
+# b.reverse()
+# print(min(b))
+# print(b)
+c =[1, 22, 32, 5, 6, 8, 11, 12]
+# c.sort(reverse=True)
+# c.reverse()
+# print(c)
+# print(c[1])
+# print(c[-1])
+
+a = c[0:-1:2]
+print(a)
+
+
+# c = [4, 5, 6]
+# b.extend(c)
+# b.append('d')
+# b.pop()
+# b.pop(1)
+# del b[1]
+# c = b.count(8)
+# print(b)
+# print(c)
+# print(len(b))
+
+
+
+
+
